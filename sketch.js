c8eac5663b3fec492e6d16f1ca1ad6079ec2bca2
Gabrielvitor123 <00001118417550sp@al.educacao.sp.gov.br>
2024-08-15 16:50:37 -0300

    salvando projeto

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 400);
+    background("orange")
+  }
+  
+  function draw() {
+    stroke("blue")
+    fill("blue")
+    
+    
+    if (mouseIsPressed) {   
+    rect(mouseX, mouseY, 20, 35);  
+    }
+  }
